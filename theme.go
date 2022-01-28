d218b7f76ef1f53181d8fae0c7c9266caf2ea452
core3545 <2424284164@qq.com>
2022-01-28 22:19:30 +0800

    update theme

--- theme.go
+++ theme.go
@@ -11,31 +11,21 @@ type myTheme struct{}
 
 var _ fyne.Theme = (*myTheme)(nil)
 
-// return bundled font resource
-func (*myTheme) Font(s fyne.TextStyle) fyne.Resource {
-	if s.Monospace {
-		return theme.DefaultTheme().Font(s)
-	}
-	if s.Bold {
-		if s.Italic {
-			return theme.DefaultTheme().Font(s)
-		}
-		return resourceSarasaMonoScNerdRegularTtf
-	}
-	if s.Italic {
-		return theme.DefaultTheme().Font(s)
-	}
+func (m myTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	return theme.LightTheme().Color(name, variant)
+}
+
+func (m myTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(name)
+}
+
+func (m myTheme) Font(style fyne.TextStyle) fyne.Resource {
 	return resourceSarasaMonoScNerdRegularTtf
 }
 
-func (*myTheme) Color(n fyne.ThemeColorName, v fyne.ThemeVariant) color.Color {
-	return theme.DefaultTheme().Color(n, v)
-}
-
-func (*myTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
-	return theme.DefaultTheme().Icon(n)
-}
-
-func (*myTheme) Size(n fyne.ThemeSizeName) float32 {
-	return theme.DefaultTheme().Size(n)
+func (m myTheme) Size(name fyne.ThemeSizeName) float32 {
+	if name == theme.SizeNameText {
+		return 16
+	}
+	return theme.DefaultTheme().Size(name)
 }
